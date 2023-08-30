6f9dfb27aa36bdb083c92d1e170d43dd76c0cfa1
PhantomNinja0 <126066407+PhantomNinja0@users.noreply.github.com>
2023-08-30 10:34:28 +0200

    Delete JadinMitchellPodcastPlateau directory

--- JadinMitchellPodcastPlateau/src/components/SupabaseClient.jsx
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://zfxzahzhfavlgrhwxoqr.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmeHphaHpoZmF2bGdyaHd4b3FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIzNTE5ODQsImV4cCI6MjAwNzkyNzk4NH0.BM9uNGWwbGL3dfBmkK3kk1JX9RJsaOsC3E3NbkdPtOY';
-
-// Create the Supabase client using the provided URL and key
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- JadinMitchellPodcastPlateau/src/config/SupabaseClient.jsx
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://zfxzahzhfavlgrhwxoqr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmeHphaHpoZmF2bGdyaHd4b3FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIzNTE5ODQsImV4cCI6MjAwNzkyNzk4NH0.BM9uNGWwbGL3dfBmkK3kk1JX9RJsaOsC3E3NbkdPtOY';
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
